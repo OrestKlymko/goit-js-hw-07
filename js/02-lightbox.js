79dd41b9edd9ced0317fd6f23d6964b4d041276f
OrestKlymko <orest.klymko2020@gmail.com>
2023-02-19 10:46:52 +0100

    upd 2_task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,21 +14,12 @@ gallery.insertAdjacentHTML("afterbegin",renderIt);
 
 console.log(galleryItems);
 
+let lightbox = new SimpleLightbox('.gallery a', {
+	spinner: true,
+	enableKeyboard: true,
+	captionsData: "alt",
+	captionDelay: 250,
+});
 
-gallery.addEventListener('click',onChangeSize);
-function onChangeSize(e) {
-	e.preventDefault();
-	if (e.target.nodeName !== "IMG") {
-		return;
-	}
-	let lightbox = new SimpleLightbox('.gallery a', {
-		spinner: true,
-		enableKeyboard: true,
-		captionType:'text',
-		captionSelector: 'figcaption', //не міг достукатись через атрибути до альту (не знаю як а в неті інфи не знайшов просто, тому вирішив піти таким способом
-		captionsData: `${e.target.alt}`,
-		captionDelay: 250,
-	});
+lightbox.on((e)=>	e.preventDefault());
 
-
-}
